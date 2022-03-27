891fe9d1e18f635bca1a27a3e92d13ad1f7492f1
abrenzink <a.brenzink@gmail.com>
2022-03-26 23:07:36 -0400

    week05

--- week05/scripts/task5.js
+++ week05/scripts/task5.js
@@ -122,9 +122,15 @@ function sortBy (){
                 (temple1, temple2) => {
                     let templeName1 = temple1.templeName.toLowerCase();
                     let templeName2 = temple2.templeName.toLowerCase();
-                    if (templeName1 < templeName2) return -1;
-                    else if (templeName1 > templeName2) return 1;
-                    else return 0;
+                    if (templeName1 < templeName2) {
+                        return -1;
+                    }
+                    else if (templeName1 > templeName2) {
+                        return 1;
+                    }
+                    else {
+                        return 0;
+                    }
                 }));
             break;
         case 'templeNameDescending':
@@ -132,9 +138,15 @@ function sortBy (){
                 (temple1, temple2) => {
                     let templeName1 = temple1.templeName.toLowerCase();
                     let templeName2 = temple2.templeName.toLowerCase();
-                    if (templeName1 > templeName2) return -1;
-                    else if (templeName1 < templeName2) return 1;
-                    else return 0;
+                    if (templeName1 > templeName2) {
+                        return -1;
+                    }
+                    else if (templeName1 < templeName2) {
+                        return 1; 
+                    }
+                    else {
+                        return 0;
+                    }
                 }));
             break;
         default:
